983a07705b60df30ca09746ee8b5e1b923d06fe4
mahmud2137 <54813199+mahmud2137@users.noreply.github.com>
2019-11-11 13:14:48 -0500

    Basic LSTM model
    Not learning at all. Loss is same all over the epochs.

---
+++ LSTM.py
@@ -0,0 +1,45 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Softmax, Dropout
+# from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import pandas as pd
+from data_preprocessing import *
+
+# Data reading and preparing
+walking = reading_sensor_values('walking.txt')
+seating  = reading_sensor_values(('seating.txt'))
+
+walking = downsample_n_segment(walking)
+seating = downsample_n_segment(seating)
+X = np.append(walking,seating, axis=0)
+y = np.append(np.zeros(walking.shape[0]),np.ones(seating.shape[0])) # 0 is for walking and 1 is for seating
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+
+scalers = {}
+for i in range(X_train.shape[2]):
+    scalers[i] = MinMaxScaler()
+    X_train[:, :, i] = scalers[i].fit_transform(X_train[:, :, i]) 
+
+for i in range(X_test.shape[2]):
+    X_test[:, :, i] = scalers[i].transform(X_test[:, :, i]) 
+
+# print(X_train[0])
+# print(X)
+
+input_shape = X_train.shape[1:3]
+model = Sequential()
+model.add(LSTM(1000, input_shape=input_shape, return_sequences=True))
+model.add(LSTM(100))
+model.add(Dense(100, activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1))
+model.add(Softmax())
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.summary()
+model.fit(X_train, y_train, epochs=100, batch_size=32, verbose=2)
+
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,6 +6,7 @@ import re
 def reading_sensor_values(path):
     started = False
     sensor_values = []
+    intensity_values = []
     with open(path, "r") as fp:
         for i, line in enumerate(fp.readlines()):
             if line.rstrip() == "[scan]": 
@@ -16,11 +17,29 @@ def reading_sensor_values(path):
                 continue
             # process line 
             if started:
-                values = re.findall(f'\d+', line)[::2]
-                values = [int(x) for x in values]
-                sensor_values.append(values)
+                numbers = re.findall(f'\d+', line) 
+                distances = numbers[::2]
+                intensities = numbers[1::2]
+                distances = [int(x) for x in distances]
+                intensities = [int(x) for x in intensities]
+                distances.extend(intensities)
+                sensor_values.append(distances)
+                # intensity_values.append(intensities)
 
-    return np.array(sensor_values)
+    return np.array(sensor_values)#, np.array(intensity_values)
 
-sv = reading_sensor_values('walking.txt')
-print(sv.shape)
+def downsample_n_segment(data, down_sample_factor=2, segment_size = 40):
+    data = data[::down_sample_factor] #downsample to 20 frames per second
+    n_segments = data.shape[0]//segment_size
+    data = data[0:n_segments*segment_size]
+    data_segmented = data.reshape(-1,segment_size,data.shape[1])
+
+    return data_segmented
+
+if __name__ == "__main__":
+    walking = reading_sensor_values('walking.txt')
+    walking_segmented = downsample_n_segment(walking)
+    print(walking_segmented.shape)
+    
+
+
