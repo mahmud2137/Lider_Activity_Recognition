f1daee41f49dbd0b16d048781ac44381db1ecbcf
mahmud2137 <54813199+mahmud2137@users.noreply.github.com>
2019-11-13 13:15:51 -0500

    Working Convolution LSTM model
    Very Low test loss

--- Cov_LSTM.py
+++ Cov_LSTM.py
@@ -2,6 +2,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Softmax, Dropout, Conv1D, TimeDistributed, MaxPooling1D, Flatten
 # from keras.layers import LSTM
+from keras.utils import to_categorical
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import mean_squared_error, accuracy_score
 from sklearn.model_selection import train_test_split
@@ -19,6 +20,8 @@ X = np.append(walking,seating, axis=0)
 y = np.append(np.zeros(walking.shape[0]),np.ones(seating.shape[0])) # 0 is for walking and 1 is for seating
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 X_train.shape
 
 scalers = {}
@@ -31,22 +34,29 @@ for i in range(X_test.shape[2]):
 
 # print(X_train[0])
 # print(X)
+X_train = np.expand_dims(X_train, axis=3)
+X_test = np.expand_dims(X_test, axis=3)
 
-input_shape = X_train.shape[1:3]
+
+input_shape = X_train.shape[1:]
+
 model = Sequential()
-model.add(TimeDistributed(Conv1D(filters=5, kernel_size=3, activation='relu'),input_shape=input_shape)
+model.add(TimeDistributed(Conv1D(filters=5, kernel_size=3, activation='relu'),input_shape=input_shape))
 model.add(TimeDistributed(MaxPooling1D(pool_size = 2)))
 model.add(TimeDistributed(Flatten()))
-model.add(LSTM(100, stateful = True, return_sequences=True))
-model.add(LSTM(50, stateful = True))
-model.add(Dense(20, activation = 'relu'))
-model.add(Dropout(0.2))
-model.add(Dense(1))
+model.add(LSTM(200, return_sequences=True))
+model.add(LSTM(100))
+model.add(Dense(50, activation = 'relu'))
+model.add(Dropout(0.1))
+model.add(Dense(2))
 model.add(Softmax())
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(loss='categorical_crossentropy', optimizer='RMSprop')
 model.summary()
 
 hist = model.fit(X_train, y_train, epochs=100, batch_size=32, verbose=2)
-y_pred = model.predict(X_train)
-test_acc = accuracy_score(y_test, y_pred)
-print(test_acc)
+# y_pred = model.predict(X_test)
+test_score = model.evaluate(X_test, y_test, verbose=1)
+# test_acc = accuracy_score(y_test, y_pred)
+print(test_score)
+plt.plot(hist.history['loss'])
+plt.savefig('loss_curve.png')
